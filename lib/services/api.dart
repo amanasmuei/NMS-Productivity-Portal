12ebfac1fceb22835010ae07d5c4bf082076aef4
amanasmuei <arahman.asmuei@tm.com.my>
2021-08-20 16:36:31 +0800

    update

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -2,23 +2,23 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:nms_productivity_portal/models/login.dart';
-import 'package:nms_productivity_portal/models/product.dart';
+// import 'package:nms_productivity_portal/models/product.dart';
 
 class Api {
   static var client = http.Client();
   static var _baseURL = "http://10.49.39.205:8181/api";
 
-  static Future<List<Product>> fetchProducts() async {
-    var response = await client.get(Uri.parse(
-        'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'));
-    if (response.statusCode == 200) {
-      var jsonString = response.body;
-      return productFromJson(jsonString);
-    } else {
-      //show error message
-      return null;
-    }
-  }
+  // static Future<List<Product>> fetchProducts() async {
+  //   var response = await client.get(Uri.parse(
+  //       'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'));
+  //   if (response.statusCode == 200) {
+  //     var jsonString = response.body;
+  //     return productFromJson(jsonString);
+  //   } else {
+  //     //show error message
+  //     return null;
+  //   }
+  // }
 
   static Future<List> login({String staffId, String password}) async {
     
@@ -35,6 +35,46 @@ class Api {
     print(responseJson);
 
     return [responseJson];
+    
+    // var response = await client.post(Uri.parse('$_baseURL/auth/login'),
+    //     headers: <String, String>{
+    //       'Content-Type': 'multipart/form-data;',
+    //     },
+    //     body: jsonEncode(
+    //         <String, String>{"staff_id": staffId, "staff_password": password}));
+
+    // if (response.statusCode == 200) {
+    //   var json = response.body;
+    //   print(json);
+    //   // var loginRes = loginRespFromJson(json);
+    //   // return [loginRes];
+    // } else {
+    //   var json = response.body;
+    //   print(json);
+    //   var errorResp = loginRespFromJson(json);
+    //   return [errorResp];
+    // }
+  }
+
+  static Future<List> activity({String month , String year}) async {
+    
+    var url = Uri.parse('$_baseURL/activity');
+    var response = await client.post(url, 
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(
+            <String, String>{"month": month, "year": year})
+    );
+
+
+    // var streamedResponse = await request.send();
+    // var response = await http.Response.fromStream(streamedResponse);
+    // print(response.body);
+    // final responseJson = json.decode(response.body);
+    // print(responseJson);
+
+    // return [responseJson];
     
     // var response = await client.post(Uri.parse('$_baseURL/auth/login'),
     //     headers: <String, String>{
